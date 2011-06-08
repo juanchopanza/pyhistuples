e42d2c0af7f6a1117fe65b43f9ebb767386e4b84
Juan Palacios <juan.palacios.puyana@gmail.com>
2011-06-08 08:42:57 +0200

    Add setup files for distutils.

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Pyhistuples',
+      version='1.0',
+      description='Python Histogram and NTuple',
+      author='Juan Palacios',
+      author_email='juan.palacios.puyana@gmail.com',
+      url='https://github.com/juanchopanza/pyhistuples',
+      license = 'LGPL',
+      packages=['pyhistuples',
+                'pyhistuples.pyhistogram',
+                'pyhistuples.pyntuple'],
+      requires = ['matplotlib', 'pytest'],
+      data_files = [ 'COPYING', 'COPYING.LESSER'],
+     )
